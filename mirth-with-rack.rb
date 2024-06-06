57c19e36c7c43fabc9f739551849b46c81d3d478
Alvaro Padilla <varoboss.ap@gmail.com>
2024-06-05 18:10:25 -0600

    Add Rack::Response to handle http responses

--- mirth-with-rack.rb
+++ mirth-with-rack.rb
@@ -3,24 +3,24 @@
 #   follows the Rack specifications and uses
 #   Rack::Request to handle requests
 # * Use Puma as an application server
-
 require 'yaml/store'
 
 # Require the relevant libraries
 require 'rack/handler/puma'
+require 'rack'
 
 app = -> environment {
   # Create an application Server and New Rack::Request Object
   request = Rack::Request.new(environment)
+  response = Rack::Response.new
 
   store = YAML::Store.new("mirth.yml")
 
   # Use Built-in Rack::Response Methods
   if request.get? && request.path == "/show/birthdays"
-    status = 200
-    content_type = "text/html"
-    response_message = "<body style='background: black; color: white;'>\n"
-    response_message << "<ul>\n"
+    response.content_type = "text/html; charset=UTF-8"
+    response.write "<body style='background: black; color: white;'>\n"
+    response.write "<ul>\n"
 
     # Get all the birthdays data in a hash object
     all_birthdays = {}
@@ -29,23 +29,19 @@ app = -> environment {
     end
 
     all_birthdays.each do |birthday|
-      response_message << "<li> #{birthday[:name]}</b> was born on #{birthday[:date]}!</li>\n"
+      response.write "<li> #{birthday[:name]}</b> was born on #{birthday[:date]}!</li>\n"
     end
 
-    response_message << "</ul>\n"
-    response_message << <<~SRT
+    response.write "</ul>\n"
+    response.write <<~SRT
       <form action="/add/birthday" method="post" enctype="application/x-www-form-urlencoded">
         <p><label>Name <input type="text" name="name"></label></p>
         <p><label>Birthday <input type="date" name="date"></label></p>
         <p><button>Submit birthday</button></p>
       </form>
     SRT
-    response_message << "</body>\n"
+    response.write "</body>\n"
   elsif request.post? && request.path == "/add/birthday"
-    status = 303
-    content_type = "text/html"
-    response_message = ""
-
     new_birthday = request.params
 
     # Store the user-input birthday data
@@ -53,19 +49,13 @@ app = -> environment {
     store.transaction do
       store[:birthdays] << new_birthday.transform_keys(&:to_sym)
     end
+    response.redirect('/show/birthdays', 303)
   else
-    status = 200
-    content_type = "text/plain"
-    response_message = "Received a #{request.request_method} request to #{request.path}"
+    response.content_type = "text/plain; charset=UTF-8"
+    response.write("Received a #{request.request_method} request to #{request.path}")
   end
 
-  # Return 3-element Array
-  headers = {
-    'Content-Type' => "#{content_type}; charset=#{response_message.encoding.name}",
-    "Location" => "/show/birthdays"
-  }
-  body = [response_message]
-  [status, headers, body]
+  response.finish
 }
 
 Rack::Handler::Puma.run(app, :Port => 1337, :Verbose => true)
